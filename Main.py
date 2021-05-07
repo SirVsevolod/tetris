ac614afd3fd57eb2e0d29385e491e61dae052f16
SirVsevolod <19vsevolod97@gmail.com>
2021-05-07 16:45:05 +0300

    second

--- Main.py
+++ Main.py
@@ -1,10 +1,12 @@
 from time import perf_counter
 import tetris
-
+import tracemalloc
 
 if __name__ == '__main__':
     a = [(3, 5), [((2, 2), 1)], [((3, 2), 1), ((2, 2), 2)]]
+    tracemalloc.start()
     start = perf_counter()
     tetris.Main(a)
     end = perf_counter()
-    #print(str (end - start) + ' time')
+    print(str (end - start) + ' time')
+    print("Current: %d, Peak %d" % tracemalloc.get_traced_memory())
